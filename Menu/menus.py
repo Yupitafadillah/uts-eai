f57fe62dffda02eda6a1ab8f63282f054f5c13a5
Annisawida <annisawida@student.telkomuniversity.ac.id>
2024-04-29 10:11:26 +0700

    Commit Menu

--- Menu/menus.py
+++ Menu/menus.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 menus = [
-    {"product_id": 1, "name":"Bacon, Egg & Cheese Biscuit", "price":"3.23", "img":"https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Bacon-Egg-Cheese-Biscuit-Regular-Size-Biscuit-1:1-4-product-tile-desktop"},
+    {"product_id": 1, "name":"Bacon, Egg and Cheese Biscuit", "price":"3.23", "img":"https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Bacon-Egg-Cheese-Biscuit-Regular-Size-Biscuit-1:1-4-product-tile-desktop"},
     {"product_id": 2, "name":"Egg McMuffin", "price":"5", "img":"https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Egg-McMuffin-1:1-4-product-tile-desktop"},
     {"product_id": 3, "name":"Sausage McMuffin", "price":"12", "img": "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Sausage-McMuffin:1-4-product-tile-desktop"},
     {"product_id": 4, "name":"Sausage Biscuit", "price":"12", "img": "https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Sausage-Biscuit-Regular-Size-Biscuit-1:1-4-product-tile-desktop"},
